379991fb47bdfe426790fc8d1c4772c6642c4af2
Erick Cestari <erickcestari03@gmail.com>
2025-12-04 10:36:03 -0300

    style: add formatter for lnd

--- modules/lnd/module.cpp
+++ modules/lnd/module.cpp
@@ -1,31 +1,29 @@
 #include <span>
 
-#include "module.h"
 #include "lnd_wrapper/libscript.h"
+#include "module.h"
 
-namespace bitcoinfuzz
-{
-    namespace module
-    {
-        Lnd::Lnd(void) : BaseModule("Lnd") {}
+namespace bitcoinfuzz {
+namespace module {
+Lnd::Lnd(void) : BaseModule("Lnd") {}
 
-        std::optional<std::string> Lnd::deserialize_invoice(std::string str) const
-        {
-            auto result = LndDeserializeInvoice(str.c_str());
-            std::string result_str(result);
-            free(result);
-            return result_str;
-        }
-
-        std::optional<std::string> Lnd::parse_p2p_lightning_message(std::span<const uint8_t> buffer) const
-        {
-            auto result = LndParseP2pLightningMessage(reinterpret_cast<const char *>(buffer.data()), buffer.size());
-            if (result == nullptr) {
-                return std::nullopt;
-            }
-            std::string result_str(result);
-            free(result);
-            return result_str;
-        }
-    }
+std::optional<std::string> Lnd::deserialize_invoice(std::string str) const {
+  auto result = LndDeserializeInvoice(str.c_str());
+  std::string result_str(result);
+  free(result);
+  return result_str;
 }
+
+std::optional<std::string>
+Lnd::parse_p2p_lightning_message(std::span<const uint8_t> buffer) const {
+  auto result = LndParseP2pLightningMessage(
+      reinterpret_cast<const char *>(buffer.data()), buffer.size());
+  if (result == nullptr) {
+    return std::nullopt;
+  }
+  std::string result_str(result);
+  free(result);
+  return result_str;
+}
+} // namespace module
+} // namespace bitcoinfuzz
--- modules/lnd/module.h
+++ modules/lnd/module.h
@@ -1,22 +1,21 @@
+#include <bitcoinfuzz/basemodule.h>
+#include <cstddef>
+#include <cstdint>
 #include <optional>
 #include <span>
 #include <vector>
-#include <cstddef>
-#include <cstdint>
-#include <bitcoinfuzz/basemodule.h>
 
-namespace bitcoinfuzz
-{
-    namespace module
-    {
-        class Lnd : public BaseModule
-        {
-        public:
-            Lnd(void);
-            std::optional<std::string> deserialize_invoice(std::string str) const override;
-            std::optional<std::string> parse_p2p_lightning_message(std::span<const uint8_t> buffer) const override;
-            ~Lnd() noexcept override = default;
-        };
+namespace bitcoinfuzz {
+namespace module {
+class Lnd : public BaseModule {
+public:
+  Lnd(void);
+  std::optional<std::string>
+  deserialize_invoice(std::string str) const override;
+  std::optional<std::string>
+  parse_p2p_lightning_message(std::span<const uint8_t> buffer) const override;
+  ~Lnd() noexcept override = default;
+};
 
-    }
-}
+} // namespace module
+} // namespace bitcoinfuzz
