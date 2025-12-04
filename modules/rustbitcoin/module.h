241a0079b7b8e4c3dc31ccbef3203f3c96683d86
Erick Cestari <erickcestari03@gmail.com>
2025-12-04 10:37:03 -0300

    style: add formatter for rustbitcoin

--- modules/rustbitcoin/module.h
+++ modules/rustbitcoin/module.h
@@ -1,29 +1,33 @@
+#include <bitcoinfuzz/basemodule.h>
+#include <cstddef>
+#include <cstdint>
 #include <optional>
 #include <span>
 #include <string>
 #include <vector>
-#include <cstddef>
-#include <cstdint>
-#include <bitcoinfuzz/basemodule.h>
 
-namespace bitcoinfuzz
-{
-    namespace module
-    {
-        class Rustbitcoin : public BaseModule
-        {
-        public:
-            Rustbitcoin(void);
-            std::optional<std::string> script_parse(std::span<const uint8_t> buffer) const override;
-            std::optional<std::string> deserialize_block(std::span<const uint8_t> buffer) const override;
-            std::optional<std::string> address_parse(std::string str) const override;
-            std::optional<std::string> psbt_parse(std::span<const uint8_t> buffer) const override;
-            std::optional<std::string> addrv2_parse(std::span<const uint8_t> buffer) const override;
-            std::optional<int> cmpctblocks_parse(std::span<const uint8_t> buffer) const override;
-            std::optional<std::string> parse_p2p_message(std::span<const uint8_t> buffer) const override;
-            std::optional<std::string> bip32_master_keygen(std::span<const uint8_t> buffer) const override;
-            ~Rustbitcoin() noexcept override = default;
-        };
+namespace bitcoinfuzz {
+namespace module {
+class Rustbitcoin : public BaseModule {
+public:
+  Rustbitcoin(void);
+  std::optional<std::string>
+  script_parse(std::span<const uint8_t> buffer) const override;
+  std::optional<std::string>
+  deserialize_block(std::span<const uint8_t> buffer) const override;
+  std::optional<std::string> address_parse(std::string str) const override;
+  std::optional<std::string>
+  psbt_parse(std::span<const uint8_t> buffer) const override;
+  std::optional<std::string>
+  addrv2_parse(std::span<const uint8_t> buffer) const override;
+  std::optional<int>
+  cmpctblocks_parse(std::span<const uint8_t> buffer) const override;
+  std::optional<std::string>
+  parse_p2p_message(std::span<const uint8_t> buffer) const override;
+  std::optional<std::string>
+  bip32_master_keygen(std::span<const uint8_t> buffer) const override;
+  ~Rustbitcoin() noexcept override = default;
+};
 
-    }
-}
+} // namespace module
+} // namespace bitcoinfuzz
--- modules/rustbitcoin/rust_bitcoin_lib/rust_bitcoin_lib.h
+++ modules/rustbitcoin/rust_bitcoin_lib/rust_bitcoin_lib.h
@@ -1,12 +1,15 @@
 #include <cstdint>
 
-extern "C" char* rust_bitcoin_script(const uint8_t *data, size_t len);
-extern "C" char* rust_bitcoin_des_block(const uint8_t *data, size_t len);
-extern "C" char* rust_bitcoin_script_asm(const char* input);
-extern "C" char* rust_bitcoin_address_parse(const char* address);
-extern "C" char* rust_bitcoin_psbt_parse(const uint8_t *data, size_t len);
-extern "C" char* rust_bitcoin_addrv2(const uint8_t *data, size_t len);
-extern "C" int32_t rust_bitcoin_cmpctblocks_parse(const uint8_t *data, size_t len);
-extern "C" void free_c_string(char* ptr);
-extern "C" char* rust_bitcoin_parse_p2p_message(const uint8_t *data, size_t len);
-extern "C" char* rust_bitcoin_bip32_master_keygen(const uint8_t *data, size_t len);
+extern "C" char *rust_bitcoin_script(const uint8_t *data, size_t len);
+extern "C" char *rust_bitcoin_des_block(const uint8_t *data, size_t len);
+extern "C" char *rust_bitcoin_script_asm(const char *input);
+extern "C" char *rust_bitcoin_address_parse(const char *address);
+extern "C" char *rust_bitcoin_psbt_parse(const uint8_t *data, size_t len);
+extern "C" char *rust_bitcoin_addrv2(const uint8_t *data, size_t len);
+extern "C" int32_t rust_bitcoin_cmpctblocks_parse(const uint8_t *data,
+                                                  size_t len);
+extern "C" void free_c_string(char *ptr);
+extern "C" char *rust_bitcoin_parse_p2p_message(const uint8_t *data,
+                                                size_t len);
+extern "C" char *rust_bitcoin_bip32_master_keygen(const uint8_t *data,
+                                                  size_t len);
