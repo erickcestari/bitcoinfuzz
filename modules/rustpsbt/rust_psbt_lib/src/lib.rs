9fa30ad723216909034070b88170394eed4fc235
Erick Cestari <erickcestari03@gmail.com>
2026-03-02 11:12:20 -0300

    add rust-psbt

--- modules/rustpsbt/rust_psbt_lib/src/lib.rs
+++ modules/rustpsbt/rust_psbt_lib/src/lib.rs
@@ -3,6 +3,7 @@ use std::os::raw::c_char;
 use std::slice;
 
 use psbt_v2::bitcoin::Psbt;
+use psbt_v2::bitcoin::hex::DisplayHex;
 
 unsafe fn str_to_c_string(input: &str) -> *mut c_char {
     CString::new(input).unwrap().into_raw()
@@ -29,40 +30,99 @@ pub unsafe extern "C" fn rust_psbt_psbt_parse(data: *const u8, len: usize) -> *m
     match Psbt::deserialize(data_slice) {
         Ok(psbt) => {
             let mut result = String::new();
+            let tx = psbt.unsigned_tx;
 
-            //result.push_str(&format!("v={};", psbt.unsigned_tx.version));
-            result.push_str(&format!("lt={};", psbt.unsigned_tx.lock_time));
-            result.push_str(&format!("in={};", psbt.inputs.len()));
-            result.push_str(&format!("out={};", psbt.outputs.len()));
-            for (i, input) in psbt.unsigned_tx.input.iter().enumerate() {
-                if i < psbt.inputs.len() {
-                    result.push_str(&format!(
-                        "in{}prev={}:{};",
-                        i, input.previous_output.txid, input.previous_output.vout
-                    ));
-                    result.push_str(&format!("in{}seq={};", i, input.sequence));
+            result.push_str(&format!("tx.version={};", tx.version));
+            result.push_str(&format!("tx.locktime={};", tx.lock_time));
+            result.push_str(&format!("tx.inputs={};", tx.input.len()));
+            result.push_str(&format!("tx.outputs={};", tx.output.len()));
 
-                    let psbt_input = &psbt.inputs[i];
+            result.push_str(&format!("psbt.inputs={};", psbt.inputs.len()));
+            result.push_str(&format!("psbt.outputs={};", psbt.outputs.len()));
 
-                    if psbt_input.witness_utxo.is_some() || psbt_input.non_witness_utxo.is_some() {
-                        result.push_str(&format!("in{}utxo=1;", i));
+            result.push_str("tx_inputs=[");
+            for input in tx.input.iter() {
+                result.push_str(&format!(
+                    "{{txid={}:{};",
+                    input.previous_output.txid, input.previous_output.vout
+                ));
+                result.push_str(&format!("sequence={};", input.sequence));
+                result.push_str(&format!("scriptsig={:x};", input.script_sig));
+                result.push_str(&format!("witness_stack="));
+                for (j, element) in input.witness.iter().enumerate() {
+                    if j > 0 {
+                        result.push(',');
                     }
-
-                    result.push_str(&format!("in{}sigs={};", i, psbt_input.partial_sigs.len()));
+                    result.push_str(&element.to_lower_hex_string());
                 }
+                result.push_str(";}");
             }
+            result.push_str("];");
 
-            for (i, output) in psbt.unsigned_tx.output.iter().enumerate() {
-                if i < psbt.outputs.len() {
-                    // refer: https://github.com/bitcoinfuzz/bitcoinfuzz/issues/134#issuecomment-2884936854 for typecasting
-                    result.push_str(&format!("out{}val={};", i, output.value.to_sat() as i64));
-                    result.push_str(&format!(
-                        "out{}script={};",
-                        i,
-                        output.script_pubkey.to_hex_string()
-                    ));
+            result.push_str("psbt_inputs=[");
+            for input in psbt.inputs.iter() {
+                result.push_str(&format!(
+                    "{{redeem={};",
+                    input
+                        .redeem_script.as_ref()
+                        .map(|redeem| redeem.to_hex_string())
+                        .unwrap_or("".to_string())
+                ));
+                result.push_str(&format!(
+                    "script_sig_final={};",
+                    input
+                        .final_script_sig.as_ref()
+                        .map(|script_sig| script_sig.to_hex_string())
+                        .unwrap_or("".to_string())
+                ));
+                result.push_str(&format!(
+                    "witness_script={};",
+                    input
+                        .witness_script.as_ref()
+                        .map(|witness| witness.to_hex_string())
+                        .unwrap_or("".to_string())
+                ));
+                result.push_str("final_witness_script=");
+                if let Some(witness) = input.final_script_witness.as_ref() {
+                    for (j, element) in witness.iter().enumerate() {
+                        if j > 0 {
+                            result.push(',');
+                        }
+                        result.push_str(&element.to_lower_hex_string());
+                    }
                 }
+                result.push_str(";}");
             }
+            result.push_str("];");
+
+            result.push_str("tx_outputs=[");
+            for output in tx.output.iter() {
+                result.push_str(&format!("{{value={};", output.value.to_sat()));
+                result.push_str(&format!(
+                    "script={};}}",
+                    output.script_pubkey.to_hex_string()
+                ));
+            }
+            result.push_str("];");
+
+            result.push_str("psbt_outputs=[");
+            for output in psbt.outputs.iter() {
+                result.push_str(&format!(
+                    "{{redeem={};",
+                    output
+                        .redeem_script.as_ref()
+                        .map(|redeem| redeem.to_hex_string())
+                        .unwrap_or("".to_string())
+                ));
+                result.push_str(&format!(
+                    "witness_script={};}}",
+                    output
+                        .witness_script.as_ref()
+                        .map(|witness| witness.to_hex_string())
+                        .unwrap_or("".to_string())
+                ));
+            }
+            result.push_str("]");
 
             str_to_c_string(&result)
         }
